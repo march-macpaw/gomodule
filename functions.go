7ae6c6a986f586cc51482ec18bb9689a9265775a
Oleksandr Marchenko <march@macpaw.com>
2024-05-18 23:35:14 +0300

    Add comments

--- functions.go
+++ functions.go
@@ -1,5 +1,9 @@
 package gomodule
 
+// Add adds two int and returns the sum.
+// Implements [Addition]
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
+
 func Add(a int, b int) int {
 	return a + b
 }
